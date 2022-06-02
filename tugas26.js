bb474808be58a9174c7481c3879a7ea1aed01a73
Arif <arifajif@gmail.com>
2022-06-02 07:01:57 +0700

    tugas 26

---
+++ tugas26.js
@@ -0,0 +1,92 @@
+function panggilIsFinite(){
+    var a = (2);
+    if (isFinite(a)==true) {
+        console.log("angka",a,"NOT Infinity")
+    }
+    else {
+        console.log("angka",a,"Infinity")
+    }
+
+    var b = (39);
+    if (isFinite(b)==true) {
+        console.log("angka",b,"NOT Infinity")
+    }
+    else {
+        console.log("angka",b,"Infinity")
+    }
+
+    var c = (76);
+    if (isFinite(c)==true) {
+        console.log("angka",c,"NOT Infinity")
+    }
+    else {
+        console.log("angka",c,"Infinity")
+    }
+
+    var d = (50);
+    if (isFinite(d)==true) {
+        console.log("angka",d,"NOT Infinity")
+    }
+    else {
+        console.log("angka",d,"Infinity")
+    }
+    
+    var e = (9);
+    if (isFinite(e)==true) {
+        console.log("angka",e,"NOT Infinity")
+    }
+    else {
+        console.log("angka",e,"Infinity")
+    }
+
+    var f = (7);
+    if (isFinite(f)==true) {
+        console.log("angka",f,"NOT Infinity")
+    }
+    else {
+        console.log("angka",f,"Infinity")
+    }
+
+    var g = (41);
+    if (isFinite(g)==true) {
+        console.log("angka",g,"NOT Infinity")
+    }
+    else {
+        console.log("angka",g,"Infinity")
+    }
+
+    var h = (2);
+    if (isFinite(h)==true) {
+        console.log("angka",h,"NOT Infinity")
+    }
+    else {
+        console.log("angka",h,"Infinity")
+    }
+
+    var i = (24);
+    if (isFinite(i)==true) {
+        console.log("angka",i,"NOT Infinity")
+    }
+    else {
+        console.log("angka",i,"Infinity")
+    }
+
+    var j = (1);
+    if (isFinite(j)==true) {
+        console.log("angka",j,"NOT Infinity")
+    }
+    else {
+        console.log("angka",j,"Infinity")
+    }
+
+    var k = (16);
+    if (isFinite(k)==true) {
+        console.log("angka",k,"NOT Infinity")
+    }
+    else {
+        console.log("angka",k,"Infinity")
+    }
+
+}
+
+panggilIsFinite();
